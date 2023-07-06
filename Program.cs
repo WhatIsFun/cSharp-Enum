a1684254aa89cbf84869df82a3d633370e11c3dd
WhatIsFun <mohammed110051@gmail.com>
2023-07-06 23:16:53 +0400

    cSharp-Enum + Description

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace cSharp_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Mohd", 3283, Gender.MALE, CarType.PERMIUM);
+            User user2 = new User("Salma", 234553, Gender.FEMALE, CarType.BASIC);
+            User user3 = new User("John", 47752, Gender.OTHER, CarType.VAN);
+
+            //if (user1.gender = Gender.MALE)
+            //{
+            //    Console.WriteLine($"{user1.name} is {user1.gender}");
+            
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{user1.name} is not male");
+            //}
+            Console.WriteLine((int)Gender.MALE);
+            Console.WriteLine((int)CarType.BASIC);
+            string description = GetEnumDescription(CarType.PERMIUM);
+            Console.WriteLine(description);
+
+            int orginalValue = 300;
+            CarType type = (CarType)orginalValue;
+            Console.WriteLine(type.ToString());
+            
+        }
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            if (attributes != null && attributes.Any())
+            {
+                return attributes.First().Description;
+            }
+
+            return value.ToString();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Enum
+{
+    internal class User
+    {
+        public string name;
+        public int id;
+        public Gender gender;
+        public CarType carType;
+
+        public User(string name, int id, Gender gender, CarType carType)
+        {
+            this.name = name;
+            this.id = id;
+            this.gender = gender;
+            this.carType = carType;
+        }
+    }
+    
+    enum Gender
+    {
+        MALE,
+        FEMALE,
+        OTHER
+    }
+    enum CarType
+    {
+        [Description ("This is basic")]
+        BASIC = 100,
+        [Description("This is Permium")]
+        PERMIUM = 200,
+        [Description("This is Van")]
+        VAN = 300,
+        [Description("This is Ultra Permium")]
+        ULTRA_PERMIUM = 400,
+        [Description("This is MotorCycle")]
+        MOTORCYCLE = 50
+    }
+    
+}
