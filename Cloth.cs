e0a56cb3fca47f7f18c49b4cb1051f3d86b83d49
WhatIsFun <mohammed110051@gmail.com>
2023-07-07 00:43:00 +0400

    Task 2: ClothingSize

---
+++ Cloth.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Enum
+{
+    internal class Cloth
+    {
+        public static void colthingSize()
+        {
+
+            Console.WriteLine("Enter your cloth size number in EU >> ");
+            //string size = Console.ReadLine();
+            int orginalSize = Convert.ToInt32(Console.ReadLine());
+            ClothingSize vSize = (ClothingSize)orginalSize;
+
+
+            if (vSize == ClothingSize.Small)
+            {
+                Console.WriteLine("Your size is 36 in UE which is Small");
+            }
+            else if (vSize == ClothingSize.Medium)
+            {
+                Console.WriteLine("Your size is 38 in UE which is Medium");
+            }
+            else if (vSize == ClothingSize.Large)
+            {
+                Console.WriteLine("Your size is 40 in UE which is Large");
+            }
+            else if (vSize == ClothingSize.XLarge)
+            {
+                Console.WriteLine("Your size is 42 in UE which is XLarge");
+            }
+            else if (vSize == ClothingSize.XXLarge)
+            {
+                Console.WriteLine("Your size is 44 in UE which is XXLarge") ;
+            }
+            else { Console.WriteLine("Invalid input ~`~"); }
+    }
+    }
+
+    enum ClothingSize
+    {
+        Small= 36,
+        Medium = 38,
+        Large = 40,
+        XLarge = 42,
+        XXLarge = 44
+    }
+}
--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ namespace cSharp_Enum
             int orginalValue = 300;
             CarType type = (CarType)orginalValue;
             Console.WriteLine(type.ToString());
+
             // Task 1:Create an enum called DaysOfWeek to represent the days of the
             // week (Sunday, Monday, Tuesday, etc.). Write a program that prompts the
             // user to enter a day of the week and displays a message based on the entered value.
@@ -38,8 +39,11 @@ namespace cSharp_Enum
             Console.WriteLine("\r\nTask 1: DaysOfWeek\r\n~~~~~~~~~~~~~~~");
             DayOfWeek.weekDays();
 
-            
-            
+            // Task 2: Create an enum called ClothingSize to represent different clothing sizes (Small, Medium, Large, etc.).
+            // Write a program that prompts the user to enter their clothing size and displays a message based on the entered value.
+            // Use an if-else statement to handle different cases.
+            Console.WriteLine("\r\nTask 2: ClothingSize\r\n~~~~~~~~~~~~~~~");
+            Cloth.colthingSize();
         }
 
         public static string GetEnumDescription(Enum value)
